f4db462e51a10d89ed8b1cb0872e731c10db1b83
RamenJunkie <josh_miller79@outlook.com>
2024-11-08 13:47:08 -0600

    Updated via Simple CLI Push

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ import datetime
 from io import StringIO
 from html.parser import HTMLParser
 
+# Toggle True/false to enable posting to blogs for testing.  Will Still Output to files
+runmode = True
+
 cur_date = datetime.datetime.now().strftime(('%A %Y-%m-%d'))
-
-with open('lastrun.md', 'r') as file:
-    last_time = datetime.datetime.strptime(file.read(), "%d-%b-%Y (%H:%M:%S.%f)")
-
 current_time = datetime.datetime.now()
-hours_diff = int((current_time - last_time).total_seconds() / 3600.0)
+
+try:
+    with open('lastrun.md', 'r') as file:
+        last_time = datetime.datetime.strptime(file.read(), "%d-%b-%Y (%H:%M:%S.%f)")
+        hours_diff = int((current_time - last_time).total_seconds() / 3600.0)
+except:
+    hours_diff = 0
 
 if hours_diff < 1:
     hours_diff = 1
@@ -44,6 +49,7 @@ def get_feed(feed_url):
 
 # Create the post text
 def make_post(NewsFeed, cur_blog):
+    markdown_output = ""
     # Wordpress API Point
     build_url = f'https://{cur_blog["wp_url"]}/xmlrpc.php'
     #print(build_url)
@@ -54,6 +60,7 @@ def make_post(NewsFeed, cur_blog):
     post.title = f"{cur_date} - Link List"
     post.terms_names = {'category': ['Link List'], 'post_tag': ['links', 'FreshRSS']}
     post.content = f"<p>{cur_blog['blogtitle']} Link List for {cur_date}</p>"
+    markdown_output = f"------------------------------------------------------------\n{cur_blog['blogtitle']} Link List for {cur_date}\n------------------------------------------------------------\n\n"
     # Insert Each Feed item into the post with it's posted date, headline, and link to the item.  And a brief summary from the RSS
     for each in NewsFeed.entries:
         try:
@@ -64,35 +71,39 @@ def make_post(NewsFeed, cur_blog):
             post_thumbnail = each.media_thumbnail[0]['url']
         #print(post_thumbnail)
 
-        if len(strip_tags(each.summary)) > 100:
-            post_summary = strip_tags(each.summary)[0:100]
+        post_content = strip_tags(each.summary)
+
+        if len(post_content) > 100:
+            post_summary = post_content[0:100]
         else:
-            post_summary = strip_tags(each.summary)
+            post_summary = post_content
         post.content += f'<div class="link_list_card">' \
                         f'<div class="link_card_image"><img src="{post_thumbnail}" class="link_card_image_thumb" height="150" alt="link image"></div>' \
                         f'<span class="link_list_date">{each.published[5:-15].replace(" ", "-")}</span> - <a class="link_list_link" href="{each.links[0].href}">{each.title}</a></p>' \
                         f'<p><span class="link_list_summary_title">Brief Summary:</span> <span class="link_list_summary">"{post_summary}"</span></p>' \
                         f'</div>'
+        markdown_output += f'{each.published[5:-15].replace(" ", "-")} - [{each.title}]({each.links[0].href})\n\n{post_content}\n\n------------------------------------------------------------\n\n' 
         # print(each.summary_detail.value)
         #print(each)
 
-    # Create the actual post.
-    post.post_status = 'publish'
+    if runmode:
+        # Create the actual post.
+        post.post_status = 'publish'
     #print(post.content)
-    # For Troubleshooting and reworking, uncomment the above then comment out the below, this will print results instead of posting
-    post.id = wp.call(NewPost(post))
+        # For Troubleshooting and reworking, uncomment the above then comment out the below, this will print results instead of posting
+        post.id = wp.call(NewPost(post))
+        try:
+            if post.id:
+                post.post_status = 'publish'
+                call(posts.EditPost(post.id, post))
+        except:
+            pass
+            #print("Error creating post.")
 
-    with open(f"{cur_date}.md", 'a') as filewrite:
-        filewrite.write(post.content)
-        filewrite.write("\n\n\n ---------------- \n\n\n")
+    with open(f"archive/{cur_date.split(" ")[1]}-Saved_Stories.md", 'a') as filewrite:
+        filewrite.write(markdown_output)
+        filewrite.write("\n\n\n")
 
-    try:
-        if post.id:
-            post.post_status = 'publish'
-            call(posts.EditPost(post.id, post))
-    except:
-        pass
-        #print("Error creating post.")
 
 #Get the news feed
 for each in blogs:
@@ -102,5 +113,6 @@ for each in blogs:
         make_post(newsfeed, each)
         #print(NewsFeed.entries)
 
-with open('lastrun.md', 'w') as file:
-    file.write(current_time.strftime("%d-%b-%Y (%H:%M:%S.%f)"))
+if runmode:
+    with open('lastrun.md', 'w') as file:
+        file.write(current_time.strftime("%d-%b-%Y (%H:%M:%S.%f)"))
